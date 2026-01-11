754f0d9b880ec20a082d5941478d58b107f90b93
bchiyanike <bchiyanike@gmail.com>
2026-01-11 11:49:14 +0200

    Create MainActivity with edge-to-edge and theming
    Initial implementation of MainActivity with edge-to-edge support and theming.

---
+++ app/src/main/java/com/lionico/template/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.lionico.template
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.lionico.template.ui.theme.LionicoTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // Enables edge-to-edge drawing (behind status/nav bars)
+        enableEdgeToEdge()
+        
+        setContent {
+            LionicoTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    // Your Screen Entry Point goes here
+                }
+            }
+        }
+    }
+}
