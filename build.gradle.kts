ade1f72cbdb13b204db1c8f0013c923d05484892
bchiyanike <bchiyanike@gmail.com>
2026-01-17 23:45:13 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
-// build.gradle.kts (root)
-
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
-}
+// TEMPORARY: Replace entire root build.gradle.kts
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
+    }
+} 
