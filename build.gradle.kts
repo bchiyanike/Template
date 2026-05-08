6a237532b4c4eb2db0336b4cc656e0451a096ab0
bchiyanike <bchiyanike@gmail.com>
2026-05-08 18:32:51 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,10 @@
-// File: build.gradle.kts (ROOT PROJECT)
+// build.gradle.kts (root)
 
 plugins {
-    // Android Gradle Plugin
     alias(libs.plugins.android.application) apply false
-
-    // Kotlin
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-
-    // Dependency Injection
+    alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.hilt.android) apply false
-
-    // Kotlin Symbol Processing
     alias(libs.plugins.ksp) apply false
 }
